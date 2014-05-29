8c4c290bf9536a566679bc04517fa0b5b432207c
oliverfriedmann <oliverfriedmann@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-29 02:12:50 +0000

    Initial Commit
    git-svn-id: https://plugins.svn.wordpress.org/ziggeo/trunk@922813 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ziggeo.php
@@ -0,0 +1,17 @@
+<?php 
+    /*
+    Plugin Name: Ziggeo Video Posts and Comments
+    Plugin URI: http://ziggeo.com
+    Description: Plugin for adding video posts and video comments
+    Author: Oliver Friedmann
+    Version: 1.0
+    Author URI: http://oliverfriedmann.com
+    */
+        
+
+include_once(dirname(__FILE__) . "/src/player.php");
+include_once(dirname(__FILE__) . "/src/assets.php");
+include_once(dirname(__FILE__) . "/src/header.php");
+include_once(dirname(__FILE__) . "/src/config_required_notice.php");
+include_once(dirname(__FILE__) . "/src/settings.php");
+include_once(dirname(__FILE__) . "/src/templates.php");
